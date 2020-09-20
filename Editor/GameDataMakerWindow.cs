38eea2223089d376357829e46602fdf3c2e7a29e
Luis Oliveira <dev.oliveiras@gmail.com>
2020-09-20 18:52:14 -0300

    Added base code for the tool, and a CHANGELOG file.

---
+++ Editor/GameDataMakerWindow.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDataMakerWindow : EditorWindow
+{
+    static GameDataConfig config;
+
+    string _folder;
+    string _fileName;
+    string _extension;
+
+
+    [MenuItem("Tools/Game Data Maker")]
+    public static void StartWindow()
+    {
+        config = Resources.Load("SaveConfig") as GameDataConfig;
+        if (config == null)
+        {
+            config = CreateInstance<GameDataConfig>();
+            AssetDatabase.CreateAsset(config, "Assets/Resources/SaveConfig.asset");
+        }
+
+        GameDataMakerWindow window = GetWindow<GameDataMakerWindow>(false, "Game Data Maker");
+        window._folder = config.Folder;
+        window._fileName = config.FileName;
+        window._extension = config.Extension;
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.LabelField("Save Options");
+        EditorGUILayout.Space();
+        _folder = EditorGUILayout.TextField("Folder", _folder);
+        _fileName = EditorGUILayout.TextField("File Name", _fileName);
+        _extension = EditorGUILayout.TextField("Extension", _extension);
+
+
+        EditorGUILayout.Space();
+        if (GUILayout.Button("Save"))
+        {
+            config.Setup(_folder, _fileName, _extension);
+            Selection.activeObject = config;
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
---
+++ Runtime/GameDataConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Save Manager Config", fileName ="SaveConfig")]
+public class GameDataConfig : ScriptableObject
+{
+    [HideInInspector] [SerializeField] private string _folder;
+    [HideInInspector] [SerializeField] private string _fileName;
+    [HideInInspector] [SerializeField] private string _extension;
+
+    public string Folder { get => _folder; }
+    public string FileName { get => _fileName; }
+    public string Extension { get => _extension; }
+
+    public string FilePath
+    {
+        get
+        {
+            string fullFileName = string.Concat(_fileName, ".", _extension);
+            string fullPath = Path.Combine(Application.persistentDataPath, _folder, fullFileName);
+            return fullPath;
+        }
+    }
+
+    public void Setup(string folder,string fileName, string extension)
+    {
+        _folder = folder;
+        _fileName = fileName;
+        _extension = extension;
+    }
+}
